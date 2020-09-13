d4c95dc2c92c3afc7930217c551876550350c64a
joelmaykon <joelmaykon94@gmail.com>
2020-09-13 13:49:09 -0300

    busca do sku por  id

--- src/skuRequest.ts
+++ src/skuRequest.ts
@@ -1,8 +1,20 @@
 import { ProductSuggestion, Sku } from './types/product';
 
 
-export function getSkuProduct(sku): ProductSuggestion {
-  const sku1: Sku = { id: sku, name: 'Camisa', url: "http://camisa", imageUrl: "http//" }
+export async function getSkuProduct(sku): Promise<ProductSuggestion> {
+
+  const url = `https://hiringcoders13.myvtex.com/api/catalog_system/pvt/sku/stockkeepingunitbyid/${sku}`;
+
+  const response = await fetch(url, {
+    headers: {
+      "x-vtex-api-apptoken": "SLLNRWDRPRHLMPRJZFLFOGNGLFNQUNKYAIZHMEXHUZOUFGGBOZZZRFBLYFBLINXBHJKEQJWUIEKPSSAFZMBVNLGUGNIHCLYHCMNWVCLECEQNVMTFFVMWLBOWDQXCAPSX",
+      "x-vtex-api-appkey": "vtexappkey-hiringcoders13-FCQFKD"
+    }
+  })
+
+  const skuItem = await response.json();
+
+  const sku1: Sku = { id: skuItem.id, name: skuItem.nameComplete, url: skuItem.DetailUrl, imageUrl: skuItem.ImageUrl }
 
   const products = {
     number: 1,
