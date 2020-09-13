b3aef4f2432b9a613bd2a218771a9e7e776d1ff2
joelmaykon <joelmaykon94@gmail.com>
2020-09-13 14:50:34 -0300

    listagem de skus

--- src/skuRequest.ts
+++ src/skuRequest.ts
@@ -1,6 +1,6 @@
 import { ProductSuggestion, Sku } from './types/product';
 
-async function getSkuId(sku):Sku {
+async function getSkuId(sku): Sku {
   const url = `https://hiringcoders13.myvtex.com/api/catalog_system/pvt/sku/stockkeepingunitbyid/${sku}`;
 
   const response = await fetch(url, {
@@ -13,14 +13,32 @@ async function getSkuId(sku):Sku {
   return { id: skuItem.id, name: skuItem.nameComplete, url: skuItem.DetailUrl, imageUrl: skuItem.ImageUrl };
 }
 
+async function listAllSkus(): Promise<[]> {
+  const url = `https://hiringcoders13.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitids?page=1&pagesize=50`
+  const listSkus = await fetch(url, {
+    headers: {
+      "x-vtex-api-apptoken": "SLLNRWDRPRHLMPRJZFLFOGNGLFNQUNKYAIZHMEXHUZOUFGGBOZZZRFBLYFBLINXBHJKEQJWUIEKPSSAFZMBVNLGUGNIHCLYHCMNWVCLECEQNVMTFFVMWLBOWDQXCAPSX",
+      "x-vtex-api-appkey": "vtexappkey-hiringcoders13-FCQFKD"
+    }
+  })
+
+  const response = await listSkus.json()
+  return response;
+}
 
 export async function getSkuProduct(sku): Promise<ProductSuggestion> {
 
-  const sku1: Sku = getSkuId(sku) 
+  const skuRecebido: Sku = getSkuId(sku);
+  let skusSimilares = []
+  console.log(await listAllSkus());
+  const arraySkusSimilares = await listAllSkus().map(id => {
+    if (id != skuRecebido.id) {
+      skusSimilares.push(await getSkuId(id))
+    }); 
 
   const products = {
     number: 1,
-    skus: [sku1]
+    skus: skusSimilares
   }
 
   return {
